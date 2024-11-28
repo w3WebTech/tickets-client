485c40878244e9fa5d0b45de6179d644d10fb59f
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-11-28 18:48:59 +0530

    FIX

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ export default {
   mounted() {
     // Get values from URL query parameters
 const clientCode = this.$route.query.clientcode ? this.$route.query.clientcode : "";
-const clientName = this.$route.query.clientname ? this.$route.query.clientname.replace(/\s+/g, '') : ""; // Remove all spaces
+const clientName = this.$route.query.clientname ? this.$route.query.clientname.replace(/\s+/g, '%20') : ""; // Remove all spaces
 const clientemail = this.$route.query.clientemail ? this.$route.query.clientemail : "";
     // Do something with clientCode and clientName
     // this.clientCode = "GZ10219";
